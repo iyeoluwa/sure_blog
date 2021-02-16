85b20477ba0c7e6f36ca7f0afeb5be8ce6c8710b
iyeoluwa <fajimiiyeoluwa@yahoo.com>
2021-02-16 23:20:30 +0100

    migrationchanged

--- database/migrations/2021_02_05_051650_add_summary_to_posts_table.php
+++ database/migrations/2021_02_05_051650_add_summary_to_posts_table.php
@@ -14,7 +14,7 @@ class AddSummaryToPostsTable extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->longText('summary')->change();
+            $table->longText('summary');
         });
     }
 
