446aa812f1fc604bf6381aa8b7f8847c4462b43a
iyeoluwa <fajimiiyeoluwa@yahoo.com>
2021-02-17 01:36:33 +0100

    writer comment out for now proper solution will be added very soon

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -62,14 +62,14 @@ class PermissionController extends Controller
 //        $developer->roles()->attach($dev_role);
 //        $developer->permissions()->attach($dev_perm);
 //
-        $manager  = new User();
-        $manager->name = 'Olufunke Fajimi';
-        $manager->email = 'olufunkefajimi@yahoo.com';
-        $manager->password = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi';
-        $manager->username = 'olufunkefajimi';
-        $manager->save();
-        $manager->roles()->attach($manager_role);
-        $manager->permissions()->attach($manager_perm);
+//        $manager  = new User();
+//        $manager->name = 'Olufunke Fajimi';
+//        $manager->email = 'olufunkefajimi@yahoo.com';
+//        $manager->password = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi';
+//        $manager->username = 'olufunkefajimi';
+//        $manager->save();
+//        $manager->roles()->attach($manager_role);
+//        $manager->permissions()->attach($manager_perm);
 
         return redirect()->back();
     }
