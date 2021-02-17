6fcd12c88a93d6453e868521e18dd9cb6b69410e
iyeoluwa <fajimiiyeoluwa@yahoo.com>
2021-02-17 21:11:29 +0100

    seeding rows2

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -11,37 +11,37 @@ class PermissionController extends Controller
 {
     public function Permission(){
         //roles
-        $dev_permission = Permission::where('slug','create-tasks')->first();
-        $manager_permission = Permission::where('slug','edit-users')->first();
-        //RoleTableSeeder.php
-        $dev_role = new Role();
-        $dev_role->slug = 'developer';
-        $dev_role->name = 'Front-end developer';
-        $dev_role->save();
-        $dev_role->permissions()->attach($dev_permission);
-
-        $manager_role = new Role();
-        $manager_role->slug = 'manager';
-        $manager_role->name =  'Assistant Manager';
-        $manager_role->save();
-        $manager_role->permissions()->attach($manager_permission);
+//        $dev_permission = Permission::where('slug','create-tasks')->first();
+//        $manager_permission = Permission::where('slug','edit-users')->first();
+//        //RoleTableSeeder.php
+//        $dev_role = new Role();
+//        $dev_role->slug = 'developer';
+//        $dev_role->name = 'Front-end developer';
+//        $dev_role->save();
+//        $dev_role->permissions()->attach($dev_permission);
+//
+//        $manager_role = new Role();
+//        $manager_role->slug = 'manager';
+//        $manager_role->name =  'Assistant Manager';
+//        $manager_role->save();
+//        $manager_role->permissions()->attach($manager_permission);
 
         // permissions
 
-//        $dev_role = Role::where('slug','developer')->first();
-//        $manager_role = Role::where('slug','manager')->first();
-//
-//        $createTasks = new Permission();
-//        $createTasks->slug = 'create-tasks';
-//        $createTasks->name = 'Create Tasks';
-//        $createTasks->save();
-//        $createTasks->roles()->attach($dev_role);
-//
-//        $editUsers = new Permission();
-//        $editUsers->slug = 'edit-users';
-//        $editUsers->name = 'Edit Users';
-//        $editUsers->save();
-//        $editUsers->roles()->attach($manager_role);
+        $dev_role = Role::where('slug','developer')->first();
+        $manager_role = Role::where('slug','manager')->first();
+
+        $createTasks = new Permission();
+        $createTasks->slug = 'create-tasks';
+        $createTasks->name = 'Create Tasks';
+        $createTasks->save();
+        $createTasks->roles()->attach($dev_role);
+
+        $editUsers = new Permission();
+        $editUsers->slug = 'edit-users';
+        $editUsers->name = 'Edit Users';
+        $editUsers->save();
+        $editUsers->roles()->attach($manager_role);
 
 
 
